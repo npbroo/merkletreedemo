b4271689ace39ce479790f3880fc5f06e8e759a1
Nathan <npbroo.biz@outlook.com>
2022-07-09 12:59:36 -0400

    added solidity code

---
+++ merkleTree.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MerkleTree is Ownable{
+    // --- PROPERTIES ---- //
+
+    // Calculated from `merkle_tree.js`
+    bytes32 public merkleRoot;
+    bool public worked = false;
+
+    // --- Constructor -- //
+    constructor(bytes32 _merkleRoot) {    
+        merkleRoot = _merkleRoot;    
+    }
+
+    // --- FUNCTIONS ---- //
+    function testMerkle(bytes32[] calldata _merkleProof, uint256 tokenId, uint256 rarity ) public {
+        bytes32 leaf = keccak256(abi.encodePacked(tokenId, rarity));
+        require(MerkleProof.verify(_merkleProof, merkleRoot, leaf), "Invalid Merkle Proof.");
+        worked = true;
+    }
+
+    function setMerkle(bytes32 _merkleRoot) public onlyOwner {
+        merkleRoot = _merkleRoot;
+    }
+}
